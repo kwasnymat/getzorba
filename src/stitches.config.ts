9798abe6e19332b3f184e58dfa68446c74ef06bf
Mateusz Kwasny <kwasnymat@gmail.com>
2022-11-09 09:50:14 +0100

    prettier

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,78 +1,77 @@
 import { createStitches, globalCss } from "@stitches/react";
-export const { styled } =
-  createStitches({
-    theme: {
-      colors: {
-        backgroundPrimary: "#fff",
-        backgroundSecondary: "#f2f2f2",
+export const { styled } = createStitches({
+  theme: {
+    colors: {
+      backgroundPrimary: "#fff",
+      backgroundSecondary: "#f2f2f2",
 
-        textMedium: "#6e6e6e",
-        textMediumDark: "#1c85e8",
-        textDark: "#222222",
+      textMedium: "#6e6e6e",
+      textMediumDark: "#1c85e8",
+      textDark: "#222222",
 
-        primaryText: "$textDark",
-        secondaryText: "$textMediumDark",
-        tertiaryText: "$textMedium",
+      primaryText: "$textDark",
+      secondaryText: "$textMediumDark",
+      tertiaryText: "$textMedium",
 
-        navbarPrimaryBackground: "$backgroundPrimary",
-        linkHoverColor: "$textMediumDark",
+      navbarPrimaryBackground: "$backgroundPrimary",
+      linkHoverColor: "$textMediumDark",
 
-        primaryButtonColor: "$textDark",
-        primaryButtonBackgroundColor: "$backgroundPrimary",
-        primaryButtonHoverColor: "$backgroundSecondary",
-      },
-      space: {
-        extraSmall: "0.4rem",
-        small: "0.8rem",
-        medium: "1.6rem",
-        mediumPlus: "2rem",
-        large: "2.2rem",
-        extraLarge: "3.2rem",
-
-        navbarHeight: "7.8rem",
-      },
-      sizes: {
-        navbarHeight: "7.8rem",
-      },
-      fontSizes: {
-        font10: "1rem",
-        font12: "1.2rem",
-        font14: "1.4rem",
-        font16: "1.6rem",
-        font18: "1.8rem",
-        font19: "1.9rem",
-        font20: "2rem",
-        font24: "2.4rem",
-        font25: "2.5rem",
-      },
-      fontWeights: {
-        regular: "400",
-        medium: "500",
-        semiBold: "600",
-        bold: "700",
-        extraBold: "800",
-        heavy: "900",
-      },
-      radii: {
-        small: "0.2rem",
-        medium: "0.4rem",
-        large: "0.8rem",
-        extraLarge: "1.6rem",
-      },
-      fonts: {
-        graphik: 'Graphik,"Helvetica Neue",helvetica,arial,sans-serif',
-      },
+      primaryButtonColor: "$textDark",
+      primaryButtonBackgroundColor: "$backgroundPrimary",
+      primaryButtonHoverColor: "$backgroundSecondary",
     },
-    media: {
-      "bp-xs": "(min-width: 0px)",
-      "bp-sm": "(min-width: 568px)",
-      "bp-md": "(min-width: 768px)",
-      "bp-lg": "(min-width: 992px)",
-      "bp-xl": "(min-width: 1366px)",
-      "bp-xxl": "(min-width: 1680px)",
-      dark: "(prefers-color-scheme: dark)",
+    space: {
+      extraSmall: "0.4rem",
+      small: "0.8rem",
+      medium: "1.6rem",
+      mediumPlus: "2rem",
+      large: "2.2rem",
+      extraLarge: "3.2rem",
+
+      navbarHeight: "7.8rem",
     },
-  });
+    sizes: {
+      navbarHeight: "7.8rem",
+    },
+    fontSizes: {
+      font10: "1rem",
+      font12: "1.2rem",
+      font14: "1.4rem",
+      font16: "1.6rem",
+      font18: "1.8rem",
+      font19: "1.9rem",
+      font20: "2rem",
+      font24: "2.4rem",
+      font25: "2.5rem",
+    },
+    fontWeights: {
+      regular: "400",
+      medium: "500",
+      semiBold: "600",
+      bold: "700",
+      extraBold: "800",
+      heavy: "900",
+    },
+    radii: {
+      small: "0.2rem",
+      medium: "0.4rem",
+      large: "0.8rem",
+      extraLarge: "1.6rem",
+    },
+    fonts: {
+      graphik: 'Graphik,"Helvetica Neue",helvetica,arial,sans-serif',
+    },
+  },
+  media: {
+    "bp-xs": "(min-width: 0px)",
+    "bp-sm": "(min-width: 568px)",
+    "bp-md": "(min-width: 768px)",
+    "bp-lg": "(min-width: 992px)",
+    "bp-xl": "(min-width: 1366px)",
+    "bp-xxl": "(min-width: 1680px)",
+    dark: "(prefers-color-scheme: dark)",
+  },
+});
 
 export const globalStyles = globalCss({
   "*": {
